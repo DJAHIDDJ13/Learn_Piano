3019ac5e30eeffc0a6d6f4bf0aef8af32f2a6703
root <root@localhost.localdomain>
2019-01-20 12:25:41 +0000

    login with errors

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressValidator = require('express-validator');
 
 var indexRouter = require('./routes/index');
-var loginRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressValidator());
 
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
